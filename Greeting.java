9827c85c217bb8c85377661b610b1c9fe726813d
Luke Bosse <lukebosse@u.boisestate.edu>
2018-02-27 06:43:07 -0700

    initial commit for java files

---
+++ Greeting.java
@@ -0,0 +1,13 @@
+public class Greeting {
+
+  private String greeting;
+
+  public Greeting(String greeting) {
+    this.greeting = greeting;
+  }
+
+ public void greet() {
+   System.out.println(greeting);
+ }
+
+}
---
+++ Main.java
@@ -0,0 +1,6 @@
+public class Main {
+  public static void main(String[] args) {
+    Greeting greeting = new Greeting("Hello, world!");
+    greeting.greet();
+  }
+}
