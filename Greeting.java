a873745be6c410fd89394cf91723b6c47fdfec34
Zane Walker <37115741+zanewalker@users.noreply.github.com>
2018-03-06 09:41:43 -0700

    First Time

--- Greeting.java
+++ Greeting.java
@@ -1,13 +1,14 @@
 public class Greeting {
 
-  private String greeting;
+  	private String greeting;
 
-  public Greeting(String greeting) {
-    this.greeting = greeting;
-  }
+  	public Greeting(String greeting) {
+		this.greeting = greeting;
+	}
 
- public void greet() {
-   System.out.println(greeting);
- }
+	public void greet() {
+		System.out.println(greeting);
+ 	}
 
+	public void sayHello() { System.out.println("Hello!"); }
 }
